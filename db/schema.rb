c705b1fe41f7e4ce303e7afab656790f4518cf9d
eoinsheehan <username@users.noreply.github.com>
2025-07-13 17:15:38 -0600

    fix: Update price field to float

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_12_201303) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_13_230754) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.decimal "price"
+    t.float "price"
     t.text "description"
     t.string "image_url"
     t.datetime "created_at", null: false
