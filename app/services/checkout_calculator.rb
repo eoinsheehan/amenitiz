722fae163184dc8382d07dbc4f32bba12eb30fe5
eoinsheehan <username@users.noreply.github.com>
2025-07-13 17:47:49 -0600

    feat: Add promotional logic to CheckoutCalculator

--- app/services/checkout_calculator.rb
+++ app/services/checkout_calculator.rb
@@ -1,24 +1,30 @@
 class CheckoutCalculator
-    attr_reader :items
+  attr_reader :items
 
-    def initialize(cart:)
-        @items = cart[:items]
+  def initialize(cart:)
+    @items = cart[:items]
+  end
+
+  def call
+    {items:, total_cost:}
+  end
+
+  private
+
+  def total_cost
+    items.sum do |item|
+      item_cost(item:)
     end
+  end
 
-    def call
-        {items:, total_cost:}
+  def item_cost(item:)
+    product = Product.find(item[:id])
+    case product.promotion_code
+    when "BOGO"
+      paid_quantity = (item[:quantity] / 2.0).ceil
+      product.price * paid_quantity
+    else
+      product.price * item[:quantity]
     end
-
-    private
-
-    def total_cost
-        items.sum do |item| 
-            item_cost(item:)
-        end
-    end
-
-            def item_cost(item:)
-            product = Product.find(item[:id])
-            product.price * item[:quantity]
-            end
+  end
 end
